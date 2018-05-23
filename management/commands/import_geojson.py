d4767e2b6667a4f9e6b46440e13401d61677cde8
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2018-05-23 11:41:19 +0200

    #35 - add possibility to add new features to an already existing layer

--- management/commands/import_geojson.py
+++ management/commands/import_geojson.py
@@ -12,16 +12,24 @@ class Command(BaseCommand):
     help = 'Import Features from GeoJSON files'
 
     def add_arguments(self, parser):
-        parser.add_argument('-l', '--layer',
+        exclusive_group = parser.add_mutually_exclusive_group()
+        exclusive_group.add_argument('-pk', '--layer-pk',
+                            type=int,
+                            action="store",
+                            help=("PK of the layer where to insert the"
+                                  "features.\n"
+                                  "A new layer is created if not present"))
+        exclusive_group.add_argument('-l', '--layer',
                             action="store",
                             help=("Name of created layer "
-                                  "containing GeoJSON datas"))
+                                  "containing GeoJSON datas."
+                                  "If not provided an uuid4 is set."))
         parser.add_argument('-s', '--schema',
                             type=argparse.FileType('r'),
                             action="store",
-                            required=True,
                             help=("JSON schema file that describe "
-                                  "GeoJSON properties"))
+                                  "GeoJSON properties.\n"
+                                  "Only needed if -l option is provided"))
         parser.add_argument('-g', '--geojson',
                             nargs='+',
                             type=argparse.FileType('r'),
@@ -34,24 +42,31 @@ class Command(BaseCommand):
 
     @transaction.atomic()
     def handle(self, *args, **options):
-
+        layer_pk = options.get('layer_pk', None)
         layer_name = options.get('layer') if \
             options.get('layer', None) else uuid.uuid4()
-        schema = json.loads(options.get('schema').read())
         geojson_files = options.get('geojson')
         dryrun = options.get('dry_run')
 
         sp = transaction.savepoint()
-        self.import_datas(layer_name, schema, geojson_files)
+
+        if layer_pk:
+            layer = Layer.objects.get(pk=layer_pk)
+        else:
+            try:
+                schema = json.loads(options.get('schema').read())
+            except AttributeError:
+                raise argparse.ArgumentTypeError('Please provide a valid schema file')
+            layer = Layer.objects.create(name=layer_name, schema=schema)
+
+        self.import_datas(layer, geojson_files)
 
         if dryrun:
             transaction.savepoint_rollback(sp)
         else:
             transaction.savepoint_commit(sp)
 
-    def import_datas(self, layer_name, schema, geojson_files):
-        layer = Layer.objects.create(name=layer_name, schema=schema)
-
+    def import_datas(self, layer, geojson_files):
         for file_in in geojson_files:
             geojson = file_in.read()
             layer.from_geojson(geojson)
