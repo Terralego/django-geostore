ca276bd1596a03bde7c07783807a5415d724e1f0
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2018-05-23 10:11:35 +0200

    fix linting

--- models.py
+++ models.py
@@ -21,16 +21,21 @@ class Layer(models.Model):
     group = models.CharField(max_length=255, default="__nogroup__")
     schema = JSONField(default=dict, blank=True)
 
-    def from_csv_dictreader(self, reader, pk_properties, init=False, chunk_size=1000):
+    def from_csv_dictreader(self, reader, pk_properties, init=False,
+                            chunk_size=1000):
         """Import (create or update) features from csv.DictReader object
         :param reader: csv.DictReader object
         :param pk_properties: keys of row that is used to identify unicity
-        :param init: allow to speed up import if there is only new Feature's (no updates)
-        :param chunk_size: only used if init=True, control the size of bulk_create
+        :param init: allow to speed up import if there is only new Feature's
+                    (no updates)
+        :param chunk_size: only used if init=True, control the size of
+                           bulk_create
         """
         if init:
             rl = list(reader)
-            for chunk in [rl[i:i+chunk_size] for i in range(0, len(rl), chunk_size)]:
+            chunks = [rl[i:i+chunk_size]
+                      for i in range(0, len(rl), chunk_size)]
+            for chunk in chunks:
                 entries = [
                     Feature(
                         geom=Point(),
@@ -49,7 +54,8 @@ class Layer(models.Model):
                         'layer': self,
                     },
                     layer=self,
-                    **{f'properties__{p}': row.get(p, '') for p in pk_properties}
+                    **{f'properties__{p}': row.get(p, '')
+                        for p in pk_properties}
                 )
 
     def from_geojson(self, geojson_data, from_date, to_date):
--- tests/test_import_csv_features.py
+++ tests/test_import_csv_features.py
@@ -1,17 +1,20 @@
-import tempfile
 import csv
+import tempfile
 
 from django.contrib.gis.geos import Point
 from django.test import TestCase
 
 from terracommon.terra.models import Layer
 
+
 class ImportCSVFeaturesTestCase(TestCase):
     def setUp(self):
         self.layer = Layer.objects.create(name='fake layer')
 
     def get_csv_reader_from_dict(self, fieldnames, rows):
-        self.tmp_file = tempfile.NamedTemporaryFile(mode='r+', encoding='iso-8859-15', delete=False)
+        self.tmp_file = tempfile.NamedTemporaryFile(mode='r+',
+                                                    encoding='iso-8859-15',
+                                                    delete=False)
 
         writer = csv.writer(self.tmp_file.file, )
         writer.writerow(fieldnames)
@@ -22,16 +25,18 @@ class ImportCSVFeaturesTestCase(TestCase):
 
     def test_simple_import(self):
         reader = self.get_csv_reader_from_dict(
-            ['CODGEO', 'Nb Pharmacies et parfumerie', 'Dynamique Entrepreneuriale', 'Nb Résidences Principales',
-             'Nb propriétaire', 'Nb Logement', 'Nb Résidences Secondaires', 'Nb Log Vacants',
-             'Nb Occupants Résidence Principale', 'Nb Femme', 'Nb Homme', 'Nb Mineurs', 'Nb Majeurs',
+            ['CODGEO', 'Nb Pharmacies et parfumerie',
+             'Dynamique Entrepreneuriale', 'Nb Résidences Principales',
+             'Nb propriétaire', 'Nb Logement', 'Nb Résidences Secondaires',
+             'Nb Log Vacants', 'Nb Occupants Résidence Principale',
+             'Nb Femme', 'Nb Homme', 'Nb Mineurs', 'Nb Majeurs',
              'Nb Etudiants', 'CP'],
-            [[1006, 0, 42, 41, 28, 57, 13, 3, 86, 86, 86, 101, 71, 2, 1],]
+            [[1006, 0, 42, 41, 28, 57, 13, 3, 86, 86, 86, 101, 71, 2, 1], ]
         )
 
         initial = self.layer.features.all().count()
         self.layer.from_csv_dictreader(reader, ['CODGEO'])
-        
+
         expected = initial + 1
         self.assertEqual(self.layer.features.all().count(), expected)
 
@@ -39,15 +44,21 @@ class ImportCSVFeaturesTestCase(TestCase):
 
         """Create fake features to test features reinit"""
         for i in range(2):
-            self.layer.features.create(geom=Point(), properties={'SIREN': '', 'NIC': ''}, )
+            self.layer.features.create(geom=Point(),
+                                       properties={'SIREN': '', 'NIC': ''}, )
         self.assertEqual(self.layer.features.all().count(), 2)
 
         reader = self.get_csv_reader_from_dict(
-            ['SIREN', 'NIC', 'L1_NORMALISEE', 'L2_NORMALISEE', 'L3_NORMALISEE'],
-            [['518521414', '00038', '11 RUE DU MARCHIX', '44000 NANTES', 'France'],
-             ['518521414', '00053', '52 RUE JACQUES BABINET', '31100 TOULOUSE', 'France'],
-             ['813792686', '00012', 'BOIS DE TULLE', '32700 LECTOURE', 'France'],
-             ['822869632', '00023', '52 RUE JACQUES BABINET', '31100 TOULOUSE', 'France']]
+            ['SIREN', 'NIC', 'L1_NORMALISEE', 'L2_NORMALISEE',
+             'L3_NORMALISEE'],
+            [['518521414', '00038', '11 RUE DU MARCHIX', '44000 NANTES',
+              'France'],
+             ['518521414', '00053', '52 RUE JACQUES BABINET', '31100 TOULOUSE',
+              'France'],
+             ['813792686', '00012', 'BOIS DE TULLE', '32700 LECTOURE',
+              'France'],
+             ['822869632', '00023', '52 RUE JACQUES BABINET', '31100 TOULOUSE',
+              'France']]
         )
 
         self.layer.from_csv_dictreader(reader, ['SIREN', 'NIC'], init=True)
@@ -55,8 +66,10 @@ class ImportCSVFeaturesTestCase(TestCase):
         """Init mode only create new items, it does not reset database"""
         self.assertEqual(self.layer.features.all().count(), 6)
 
-        feature = self.layer.features.get(properties__SIREN='813792686', properties__NIC='00012')
-        self.assertEqual(feature.properties.get('L1_NORMALISEE', ''), 'BOIS DE TULLE')
+        feature = self.layer.features.get(properties__SIREN='813792686',
+                                          properties__NIC='00012')
+        self.assertEqual(feature.properties.get('L1_NORMALISEE', ''),
+                         'BOIS DE TULLE')
 
     def test_create_update(self):
         self.layer.features.create(
@@ -72,9 +85,12 @@ class ImportCSVFeaturesTestCase(TestCase):
         initial = self.layer.features.all().count()
 
         reader = self.get_csv_reader_from_dict(
-            ['SIREN', 'NIC', 'L1_NORMALISEE', 'L2_NORMALISEE', 'L3_NORMALISEE'],
-            [['437582422', '00097', '52 RUE JACQUES BABINET', '31100 TOULOUSE', 'France'],
-             ['518521414', '00038', '11 RUE DU MARCHIX', '44000 NANTES', 'France']]
+            ['SIREN', 'NIC', 'L1_NORMALISEE', 'L2_NORMALISEE',
+             'L3_NORMALISEE'],
+            [['437582422', '00097', '52 RUE JACQUES BABINET', '31100 TOULOUSE',
+              'France'],
+             ['518521414', '00038', '11 RUE DU MARCHIX', '44000 NANTES',
+              'France']]
         )
 
         self.layer.from_csv_dictreader(reader, ['SIREN', 'NIC'])
@@ -82,5 +98,7 @@ class ImportCSVFeaturesTestCase(TestCase):
         expected = initial + 1
         self.assertEqual(self.layer.features.all().count(), expected)
 
-        feature = self.layer.features.get(properties__SIREN='437582422', properties__NIC='00097')
-        self.assertEqual(feature.properties.get('L1_NORMALISEE', ''), '52 RUE JACQUES BABINET')
+        feature = self.layer.features.get(properties__SIREN='437582422',
+                                          properties__NIC='00097')
+        self.assertEqual(feature.properties.get('L1_NORMALISEE', ''),
+                         '52 RUE JACQUES BABINET')
