158911a48899e86f1b0160cef38adc197cc25cde
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2018-05-28 13:53:20 +0200

    #18 - use factory boy in features tests

--- tests/factories.py
+++ tests/factories.py
@@ -1,6 +1,8 @@
 import factory
 
-from terracommon.terra.models import TerraUser
+from django.contrib.gis.geos.geometry import GEOSGeometry
+
+from terracommon.terra.models import Feature, Layer, TerraUser
 
 
 class TerraUserFactory(factory.DjangoModelFactory):
@@ -14,3 +16,28 @@ class TerraUserFactory(factory.DjangoModelFactory):
     def _create(cls, model_class, *args, **kwargs):
         manager = cls._get_manager(model_class)
         return manager.create_user(*args, **kwargs)
+
+
+class LayerFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Layer
+
+    @factory.post_generation
+    def add_features(self, create, features, **kwargs):
+        for feature in features:
+            FeatureFactory(layer=self, **feature)
+
+
+class FeatureFactory(factory.DjangoModelFactory):
+    layer = factory.SubFactory(LayerFactory)
+    geom = GEOSGeometry('''{
+        "type": "Point",
+        "coordinates": [
+          2.4609375,
+          45.583289756006316
+        ]
+      }''')
+    properties = {}
+
+    class Meta:
+        model = Feature
--- tests/test_features.py
+++ tests/test_features.py
@@ -1,59 +1,23 @@
 from datetime import date
 
-from django.contrib.gis.geos.geometry import GEOSGeometry
 from django.test import TestCase
 
-from terracommon.terra.models import Layer, TerraUser
-
+from terracommon.terra.models import Layer
+from .factories import FeatureFactory, LayerFactory
 
 class FeaturesTestCase(TestCase):
-    fake_geometry = GEOSGeometry('''{
-        "type": "Point",
-        "coordinates": [
-          2.4609375,
-          45.583289756006316
-        ]
-      }''')
-
     def setUp(self):
-        self.user = TerraUser.objects.create_user(
-            email='foo@bar.com',
-            password='123456'
-        )
-        self.layer = Layer.objects.create()
+        features_dates = [
+            {'from_date': '12-01', 'to_date': '02-01'},
+            {'from_date': '04-01', 'to_date': '05-01'},
+            {'from_date': '01-01', 'to_date': '03-01'},
+            {'from_date': '10-01', 'to_date': '12-31'},
+            {'from_date': '01-20', 'to_date': '12-20'},
+        ]
+        self.layer = LayerFactory.create(add_features=features_dates)
+
 
     def test_features_dates(self):
-        self.layer.features.create(
-            geom=self.fake_geometry,
-            properties={},
-            from_date='12-01',
-            to_date='02-01'
-        )
-        self.layer.features.create(
-            geom=self.fake_geometry,
-            properties={},
-            from_date='04-01',
-            to_date='05-01'
-        )
-        self.layer.features.create(
-            geom=self.fake_geometry,
-            properties={},
-            from_date='01-01',
-            to_date='03-01'
-        )
-        self.layer.features.create(
-            geom=self.fake_geometry,
-            properties={},
-            from_date='10-01',
-            to_date='12-31'
-        )
-        self.layer.features.create(
-            geom=self.fake_geometry,
-            properties={},
-            from_date='01-20',
-            to_date='12-20'
-        )
-
         dates = (
             (2, date(2018, 1, 5)),
             (3, date(2018, 1, 25)),
