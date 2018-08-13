6d62b5044e1f74779e8d68c829cfb21e089f7088
Simon Previdente <simon.previdente@makina-corpus.com>
2018-08-13 15:54:56 +0000

    Media accel redirect

--- helpers.py
+++ helpers.py
@@ -1,18 +1,22 @@
+from django.conf import settings
 from django.contrib.gis.geos.point import Point
 from django.http import HttpResponse, HttpResponseForbidden
 
 
-def get_media_response(request, path, permissions=None, headers=None):
+def get_media_response(request, file, permissions=None, headers=None):
     if isinstance(permissions, list):
         if not set(permissions).intersection(
                 request.user.get_all_permissions()):
             return HttpResponseForbidden()
 
     response = HttpResponse()
+    if not settings.MEDIA_ACCEL_REDIRECT:
+        response = HttpResponse(file)
     if isinstance(headers, dict):
         for header, value in headers.items():
             response[header] = value
-    response['X-Accel-Redirect'] = f'{path}'
+    if settings.MEDIA_ACCEL_REDIRECT:
+        response['X-Accel-Redirect'] = f'{file.url}'
 
     return response
 
---
+++ settings/__init__.py
@@ -0,0 +1,3 @@
+import os
+
+MEDIA_ACCEL_REDIRECT = os.getenv('MEDIA_ACCEL_REDIRECT', default=False)
