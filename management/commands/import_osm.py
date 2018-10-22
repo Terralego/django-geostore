0c05dae838bdce16b4169aa3cd0845f3284550a5
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2018-10-22 15:28:31 +0200

    Refactor import osm and test import

--- management/commands/import_osm.py
+++ management/commands/import_osm.py
@@ -5,13 +5,15 @@ import uuid
 from subprocess import call
 
 import requests
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.utils.translation import ugettext as _
 
 from terracommon.terra.models import Layer
 
 
 class Command(BaseCommand):
+    overpass_url = "http://overpass-api.de/api/interpreter"
+
     def add_arguments(self, parser):
         parser.add_argument('query', action="store",
                             help=_("Overpass query without type (out:json)"
@@ -51,26 +53,29 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         query = options.get('query')
         layer_name = options.get('layer') or uuid.uuid4()
-        type = options.get('type')
+        type_features = options.get('type')
         layer_pk = options.get('layer_pk')
         identifier = options.get('identifier')
 
-        overpass_url = "http://overpass-api.de/api/interpreter"
-        response = requests.get(overpass_url,
+        response = requests.get(self.overpass_url,
                                 params={'data': query})
         dirpath = tempfile.mkdtemp()
         with tempfile.NamedTemporaryFile(mode='wb', dir=dirpath) as tmp_osm:
             tmp_osm.write(response.content)
             tmp_geojson_path = os.path.join(dirpath, 'geojson')
-            cmd = 'ogr2ogr -f GeoJSON {} {} {}'.format(
-                tmp_geojson_path, tmp_osm.name, type
-            )
-            call(cmd, shell=True)
+            call(['ogr2ogr', '-f', 'GeoJSON', tmp_geojson_path,
+                  tmp_osm.name, type_features])
+            try:
+                tmp_geojson = open(tmp_geojson_path, 'rb')
+            except FileNotFoundError:
+                msg = 'Ogr2ogr failed to create the geojson'
+                shutil.rmtree(dirpath)
+                raise CommandError(msg)
+
             if layer_pk:
                 layer = Layer.objects.get(pk=layer_pk)
             else:
                 layer = Layer.objects.create(name=layer_name)
 
-            tmp_geojson = open(tmp_geojson_path, 'rb')
             layer.from_geojson(tmp_geojson.read(), identifier)
         shutil.rmtree(dirpath)
