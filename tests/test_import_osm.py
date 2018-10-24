b47dfb221c99af73b33ff6df0c402355fafebd2e
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2018-10-24 15:29:31 +0200

    Mock import_osm

--- tests/test_import_osm.py
+++ tests/test_import_osm.py
@@ -1,21 +1,21 @@
+from unittest import mock
+
 from django.core.management import call_command
 from django.test import TestCase
 
-from terracommon.terra.models import Layer
+from terracommon.terra.management.commands import import_osm
 
 
 class ImportgeojsonTest(TestCase):
-    def test_get_nodes_osm(self):
+    def test_import_osm(self):
         query = ("area[name='Toulouse']->.b;"
                  "(node['amenity'='bar'](area.b);way['amenity'='bar']"
                  "(area.b);relation['amenity'='bar'](area.b););out center;"
                  )
         type_feature = 'points'
-        call_command(
-            'import_osm',
-            f'{query}',
-            f'-t{type_feature}')
-
-        # Retrieve the layer
-        layer = Layer.objects.all()[0]
-        self.assertEqual('__nogroup__', layer.group)
+        with mock.patch.object(import_osm, 'Command') as mocked:
+            call_command(
+                'import_osm',
+                f'{query}',
+                f'-t{type_feature}')
+            self.assertEquals(mocked.call_count, 1)
