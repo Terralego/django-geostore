8d1b95ccfc42380f4ebc02932717ad80aeac6c32
Sebastien Corbin <seb.corbin@gmail.com>
2018-11-20 17:02:48 +0100

    Documentation for filters and remove swagger cache

--- urls.py
+++ urls.py
@@ -55,9 +55,9 @@ if settings.DEBUG:
     urlpatterns += [
         # schemas
         path('swagger/',
-             schema_view.with_ui('swagger', cache_timeout=None),
+             schema_view.with_ui('swagger', cache_timeout=0),
              name='schema-swagger-ui'),
         path('redoc/',
-             schema_view.with_ui('redoc', cache_timeout=None),
+             schema_view.with_ui('redoc', cache_timeout=0),
              name='schema-redoc'),
     ]
