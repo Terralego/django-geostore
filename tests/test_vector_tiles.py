d7d27045a7914ea8a62828c69cfba3f64d6fb365
bpg <bet.palau-gavalda@makina-corpus.com>
2019-01-06 17:19:51 +0100

    Add new test to check proper implementation

--- tests/test_vector_tiles.py
+++ tests/test_vector_tiles.py
@@ -1,10 +1,13 @@
 import json
+import os
 
 from django.core.cache import cache
+from django.core.management import call_command
 from django.db import connection
 from django.test import TestCase, override_settings
 from django.urls import reverse
 
+from terracommon.terra.models import Layer
 from terracommon.terra.tiles.helpers import (VectorTile, guess_maxzoom,
                                              guess_minzoom)
 
@@ -180,6 +183,26 @@ class VectorTilesTestCase(TestCase):
         self.assertEqual(
             guess_maxzoom(self.layer) is not None,
             True)
+
+        # test guess_maxzoom returns sensible value from OSM Fontainebleau paths&tracks
+        chunk_fontainebleau_geojson = os.path.join(os.path.dirname(__file__),
+                                                   'files',
+                                                   'chunk_fontainebleau.geojson')
+        schema_json = os.path.join(os.path.dirname(__file__),
+                                   'files',
+                                   'empty.json')
+
+        call_command(
+            'import_geojson',
+            f'-g{chunk_fontainebleau_geojson}',
+            f'-s{schema_json}',
+            f'--group=maxzoom_test',
+            '-lchunk_fontainebleau',
+            verbosity=0)
+
+        layer_chunk_fontainebleau = Layer.objects.all().get(name='chunk_fontainebleau')
+
+        self.assertEqual(guess_maxzoom(layer_chunk_fontainebleau), 13)
 
     def test_guess_minzoom(self):
 
