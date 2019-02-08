fba5c135bffd12f494388f9a4769c3d1a11b0a52
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-02-08 18:09:49 +0100

    Add tests layer_edit

--- tests/test_commands/test_layer_edit.py
+++ tests/test_commands/test_layer_edit.py
@@ -1,4 +1,5 @@
 from django.core.management import call_command
+from django.core.management.base import CommandError
 from django.test import TestCase
 
 from terracommon.terra.models import Layer
@@ -37,3 +38,75 @@ class LayerEditTest(TestCase):
         self.assertEqual('new_name', layer.name)
         self.assertEqual('new_group', layer.group)
         self.assertEqual({'foo': 'bar'}, layer.settings)
+
+    def test_layer_edit_fail_wrong_pk(self):
+        # Ensure old settings
+
+        foo_bar_json = get_files_tests('foo_bar.json')
+        # Change settings
+        with self.assertRaises(CommandError):
+            call_command(
+                'layer_edit',
+                '-pk', 999,
+                '-l', 'new_name',
+                '-gr', 'new_group',
+                '-s', foo_bar_json,
+                '-ls', foo_bar_json
+            )
+
+    def test_layer_edit_with_bad_schema(self):
+        layer = Layer.objects.first()
+        bad_json = get_files_tests('bad.json')
+        # Change settings
+        with self.assertRaises(CommandError):
+            call_command(
+                'layer_edit',
+                '-pk', layer.pk,
+                '-l', 'new_name',
+                '-gr', 'new_group',
+                '-s', bad_json,
+                '-ls', bad_json
+            )
+
+    def test_layer_edit_schema_bad_file(self):
+        layer = Layer.objects.first()
+        bad_file = get_files_tests('shapefile-RFG93.zip')
+        # Change settings
+        with self.assertRaises(CommandError):
+            call_command(
+                'layer_edit',
+                '-pk', layer.pk,
+                '-l', 'new_name',
+                '-gr', 'new_group',
+                '-s', bad_file,
+                '-ls', bad_file
+            )
+
+    def test_layer_edit_bad_settings(self):
+        layer = Layer.objects.first()
+        bad_json = get_files_tests('bad.json')
+        foo_bar_json = get_files_tests('foo_bar.json')
+        # Change settings
+        with self.assertRaises(CommandError):
+            call_command(
+                'layer_edit',
+                '-pk', layer.pk,
+                '-l', 'new_name',
+                '-gr', 'new_group',
+                '-s', foo_bar_json,
+                '-ls', bad_json
+            )
+
+    def test_layer_edit_guess_zoom(self):
+        layer = Layer.objects.first()
+        foo_bar_json = get_files_tests('foo_bar.json')
+        # Change settings
+        call_command(
+            'layer_edit',
+            '-pk', layer.pk,
+            '-l', 'new_name',
+            '-gr', 'new_group',
+            '-s', foo_bar_json,
+            '-gz'
+        )
+        self.assertEqual(str(layer.settings), "{'tiles': {'maxzoom': 15, 'minzoom': 15}}")
