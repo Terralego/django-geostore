d184560fae819586ef141aa7a9634ec18e20914d
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-02-08 18:09:20 +0100

    Change layer_edit exception, commanderror

--- management/commands/layer_edit.py
+++ management/commands/layer_edit.py
@@ -2,6 +2,7 @@ import argparse
 import json
 from json.decoder import JSONDecodeError
 
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand, CommandError
 from django.utils.translation import ugettext as _
 
@@ -15,6 +16,7 @@ class Command(BaseCommand):
         parser.add_argument('-pk', '--layer-pk',
                             type=int,
                             action="store",
+                            required=True,
                             help=_("PK of the layer to update"))
         parser.add_argument('-l', '--layer',
                             action="store",
@@ -32,9 +34,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         layer_pk = options.get('layer_pk')
-        if layer_pk:
+        try:
             layer = Layer.objects.get(pk=layer_pk)
-        else:
+        except ObjectDoesNotExist:
             raise CommandError("Please provide a valid layer pk")
 
         self._settings(layer, options)
@@ -57,7 +59,7 @@ class Command(BaseCommand):
     def _settings_settings(self, layer, layer_settings):
         try:
             layer.settings = json.loads(layer_settings.read())
-        except JSONDecodeError:
+        except (JSONDecodeError, UnicodeDecodeError):
             raise CommandError("Please provide a valid layer settings file")
 
     def _actions(self, layer, options):
