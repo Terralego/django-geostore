e4880dcc5112b697521bf6daf6a16963e42a383c
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-02-12 10:12:32 +0100

    Add test import geojson

--- tests/test_commands/test_import_geojson.py
+++ tests/test_commands/test_import_geojson.py
@@ -1,17 +1,47 @@
+from io import StringIO
+
 from django.core.management import call_command
+from django.core.management.base import CommandError
 from django.test import TestCase
 
 from terracommon.terra.models import Layer
+from terracommon.terra.tests.factories import LayerFactory
 from terracommon.terra.tests.utils import get_files_tests
 
 
 class ImportGeojsonTest(TestCase):
     def test_default_group(self):
+        output = StringIO()
         call_command(
             'import_geojson', get_files_tests('empty.json'),
-            verbosity=0)
+            verbosity=1, stdout=output)
+
         # Retrieve the layer
-        layer = Layer.objects.all()[0]
+        layer = Layer.objects.first()
+        self.assertIn(f'The created layer pk is {layer.pk}', output.getvalue())
+        self.assertEqual('__nogroup__', layer.group)
+
+    def test_default_group_nogroup_rollback(self):
+        output = StringIO()
+        call_command(
+            'import_geojson',
+            f'--dry-run', get_files_tests('empty.json'),
+            verbosity=1, stdout=output)
+        self.assertIn("The created layer pk is", output.getvalue())
+        # Retrieve the layer
+        layer = Layer.objects.all()
+        self.assertEqual(len(layer), 0)
+
+    def test_import_geojson_layer_with_bad_schema(self):
+        empty_geojson = get_files_tests('empty.json')
+        bad_json = get_files_tests('bad.json')
+
+        call_command(
+            'import_geojson',
+            '-g', empty_geojson,
+            '-s', bad_json,
+            verbosity=0)
+        layer = Layer.objects.first()
         self.assertEqual('__nogroup__', layer.group)
 
     def test_schema_generated(self):
@@ -30,3 +60,38 @@ class ImportGeojsonTest(TestCase):
             ['ALTITUDE', 'ETIQUETTE', 'HAUTEUR', 'ID', 'ID_PG', 'NATURE', 'NOM',
              'ORIGIN_BAT', 'PUB_XDECAL', 'PUB_YDECAL', 'ROTATION', 'ROTATION_S',
              'XDECAL', 'XDECAL_SYM', 'YDECAL', 'YDECAL_SYM', 'Z_MAX', 'Z_MIN', ], True)
+
+    def test_import_geojson_layer_with_bad_settings(self):
+        empty_geojson = get_files_tests('empty.json')
+        bad_json = get_files_tests('bad.json')
+        with self.assertRaises(CommandError) as error:
+            call_command(
+                'import_geojson',
+                '-g', empty_geojson,
+                '-s', empty_geojson,
+                '-ls', bad_json,
+                verbosity=0)
+        self.assertEqual("Please provide a valid layer settings file", str(error.exception))
+
+    def test_import_geojson_layer_with_pk_layer(self):
+        layer = LayerFactory()
+        self.assertEqual(len(layer.features.all()), 0)
+        geojson_sample = get_files_tests('toulouse.geojson')
+        call_command(
+            'import_geojson',
+            f'--layer-pk={layer.pk}',
+            '-g', geojson_sample,
+            verbosity=0
+        )
+        self.assertEqual(len(layer.features.all()), 838)
+
+    def test_import_geojson_layer_with_wrong_pk_layer(self):
+        geojson_sample = get_files_tests('toulouse.geojson')
+        with self.assertRaises(CommandError) as error:
+            call_command(
+                'import_geojson',
+                f'--layer-pk=999',
+                '-g', geojson_sample,
+                verbosity=0
+            )
+        self.assertIn("Layer with pk 999 doesn't exist", str(error.exception))
