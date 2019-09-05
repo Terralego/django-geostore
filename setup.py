8b38d15f481cdb97c630d92d861e9d0f547b0e85
J-E Castagnede <j.e.castagnede@gmail.com>
2019-09-05 10:02:52 +0200

    fixes for packaging

---
+++ setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+README = open(os.path.join(HERE, 'README.md')).read()
+CHANGES = open(os.path.join(HERE, 'CHANGES.md')).read()
+
+
+setup(
+    name='terra',
+    version=open(os.path.join(HERE, 'terra', 'VERSION.md')).read().strip(),
+    include_package_data=True,
+    author="Makina Corpus",
+    author_email="terralego-pypi@makina-corpus.com",
+    description='Additional template engines for Django.',
+    long_description=README + '\n\n' + CHANGES,
+    description_content_type="text/markdown",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    #url='https://github.com/Terralego/terra.backend.crud.git',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    install_requires=[
+        'django>=2.1,<2.2',
+        'djangorestframework>=3.8,<3.10', # breaking changes in 3.10
+        "djangorestframework-gis>=0.14",
+        "djangorestframework-jwt>=1.11",
+        "drf-yasg>=1.9",
+        "packaging", # wait for drf-yasg fix
+        "simpleeval>=0.9",
+        "deepmerge",
+        "weasyprint>=44",
+        "requests>=2.19",
+        "mercantile>=1.0",
+        "psycopg2>=2.7",
+        "Fiona>=1.7",
+        "python-magic>=0.4",
+        "docxtpl>=0.5",
+        "Pillow>=5.3.0",
+        "jsonschema>=3.0",
+    ],
+    extras_require={
+        'dev': [
+            'factory-boy',
+            'flake8',
+            'coverage',
+        ]
+    }
+)
---
+++ terra/settings.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+
+MEDIA_ACCEL_REDIRECT = getattr(settings, 'MEDIA_ACCEL_REDIRECT', False)
+HOSTNAME =  getattr(settings, 'HOSTNAME', '')
+TERRA_TILES_HOSTNAMES = getattr(settings, 'TERRA_TILES_HOSTNAMES', [HOSTNAME, ])
+
+SWAGGER_ENABLED = getattr(settings, 'SWAGGER_ENABLED', False)
+
+MAX_TILE_ZOOM = getattr(settings, 'MAX_TILE_ZOOM', 15)
+MIN_TILE_ZOOM = getattr(settings, 'MIN_TILE_ZOOM', 10)
+
+INTERNAL_GEOMETRY_SRID = getattr(settings, 'INTERNAL_GEOMETRY_SRID', 4326)
