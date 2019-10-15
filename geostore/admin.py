c4fb3f433b6befb63f7460e7e24bafe1e7984bed
J-E Castagnede <j.e.castagnede@gmail.com>
2019-10-15 10:54:46 +0200

    don't register admin by default

--- geostore/admin.py
+++ geostore/admin.py
@@ -4,19 +4,16 @@ from django.contrib.gis import admin
 from . import models
 
 
-@admin.register(models.Layer)
 class LayerAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'geom_type', 'layer_groups')
     list_filter = ('geom_type', 'layer_groups')
 
 
-@admin.register(models.LayerGroup)
 class LayerGroupAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
     list_filter = ('slug', )
 
 
-@admin.register(models.Feature)
 class FeatureAdmin(admin.OSMGeoAdmin):
     list_display = ('pk', 'identifier', 'layer', 'source', 'target')
     list_filter = ('layer', )
