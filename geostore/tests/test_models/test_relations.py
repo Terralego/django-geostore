e8988ac437cfde861a5926cdba3a3bb161ad9a01
J-E Castagnede <j.e.castagnede@gmail.com>
2019-12-13 16:03:54 +0100

    improve test cases

--- geostore/tests/test_models/test_relations.py
+++ geostore/tests/test_models/test_relations.py
@@ -9,16 +9,41 @@ class TestLayerRelationTestCase(TestCase):
     def setUp(self) -> None:
         self.layer_trek = LayerSchemaFactory(geom_type=GeometryTypes.LineString)
         self.layer_city = LayerSchemaFactory(geom_type=GeometryTypes.Polygon)
-        self.intersect_relation = LayerRelation.objects.create(
-            relation_type='intersects',
-            origin=self.layer_trek,
-            destination=self.layer_city,
-        )
         self.trek = FeatureFactory(layer=self.layer_trek, geom='LINESTRING(0 0, 1 1, 2 2, 3 3)')
         self.city_cover = FeatureFactory(layer=self.layer_city, geom='POLYGON((0 0, 0 3, 3 3, 3 0, 0 0))')
         self.city_uncover = FeatureFactory(layer=self.layer_city, geom='POLYGON((4 4, 4 7, 7 7, 7 4, 4 4))')
 
-    def test_sync_relations_original(self):
-        self.trek.sync_relations()
-        self.assertListEqual(list(self.trek.relations_as_origin.values_list('destination__pk', flat=True)),
+    def test_sync_relations_intersects(self):
+        intersect_relation = LayerRelation.objects.create(
+            relation_type='intersects',
+            origin=self.layer_trek,
+            destination=self.layer_city,
+        )
+        # city cover should be present after sync
+        self.trek.sync_relations(intersect_relation.pk)
+        self.assertListEqual(list(self.trek.relations_as_origin.filter(relation=intersect_relation)
+                                  .values_list('destination__pk', flat=True)),
                              [self.city_cover.pk])
+        # city cover should not be present after deletion
+        intersect_relation.delete()
+        self.assertListEqual(list(self.trek.relations_as_origin.filter(relation=intersect_relation)
+                                  .values_list('destination__pk', flat=True)),
+                             [])
+
+    def test_sync_relations_distance(self):
+        distance_relation = LayerRelation.objects.create(
+            relation_type='distance',
+            origin=self.layer_trek,
+            destination=self.layer_city,
+            settings={'distance': 1000}
+        )
+        # city cover should be present after sync
+        self.trek.sync_relations(distance_relation.pk)
+        self.assertListEqual(list(self.trek.relations_as_origin.filter(relation=distance_relation)
+                                  .values_list('destination__pk', flat=True)),
+                             [self.city_cover.pk])
+        # city cover should not be present after deletion
+        distance_relation.delete()
+        self.assertListEqual(list(self.trek.relations_as_origin.filter(relation=distance_relation)
+                                  .values_list('destination__pk', flat=True)),
+                             [])
