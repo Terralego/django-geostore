aa9d71de23888115300d3cfff0854cfd8c8b9086
J-E Castagnede <j.e.castagnede@gmail.com>
2019-12-19 17:23:04 +0100

    improve tasks

--- geostore/tasks.py
+++ geostore/tasks.py
@@ -12,9 +12,7 @@ def feature_update_relations_destinations(feature_id):
 @shared_task
 def layer_relations_set_destinations(relation_id):
     LayerRelation = apps.get_model('geostore.LayerRelation')
-    try:
-        relation = LayerRelation.objects.get(pk=relation_id)
-    except LayerRelation.DoesNotExist:
-        raise Exception('DoesNotExists', relation_id, LayerRelation)
+    relation = LayerRelation.objects.get(pk=relation_id)
+
     for feature in relation.origin.features.all():
         feature.sync_relations(relation_id)
