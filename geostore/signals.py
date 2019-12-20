7de39b05dca7ce7c05fbb0dea79df11eda0199ba
J-E Castagnede <j.e.castagnede@gmail.com>
2019-12-20 17:27:30 +0100

    no celery no sync

--- geostore/signals.py
+++ geostore/signals.py
@@ -15,12 +15,8 @@ def execute_async_func(async_func, args=()):
 def save_feature(sender, instance, **kwargs):
     if app_settings.GEOSTORE_RELATION_CELERY_ASYNC:
         execute_async_func(feature_update_relations_destinations, (instance.pk,))
-    else:
-        feature_update_relations_destinations(instance.pk)
 
 
 def save_layer_relation(sender, instance, **kwargs):
     if app_settings.GEOSTORE_RELATION_CELERY_ASYNC:
         execute_async_func(layer_relations_set_destinations, (instance.pk,))
-    else:
-        layer_relations_set_destinations(instance.pk)
--- geostore/tasks.py
+++ geostore/tasks.py
@@ -3,11 +3,11 @@ from django.apps import apps
 
 
 @shared_task
-def feature_update_relations_destinations(feature_id):
+def feature_update_relations_destinations(feature_id, relation_id=None):
     """ Update all feature layer relations as origin """
     Feature = apps.get_model('geostore.Feature')
     feature = Feature.objects.get(pk=feature_id)
-    feature.sync_relations()
+    feature.sync_relations(relation_id)
 
     return True
 
@@ -18,7 +18,7 @@ def layer_relations_set_destinations(relation_id):
     LayerRelation = apps.get_model('geostore.LayerRelation')
     relation = LayerRelation.objects.get(pk=relation_id)
 
-    for feature in relation.origin.features.all():
-        feature.sync_relations(relation_id)
+    for feature_id in relation.origin.features.values_list('pk', flat=True):
+        feature_update_relations_destinations.delay(feature_id, relation_id)
 
     return True
