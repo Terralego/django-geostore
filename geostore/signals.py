1b62947365d4b60f4be023b1e7dd209621e2d623
J-E Castagnede <j.e.castagnede@gmail.com>
2019-12-20 12:04:30 +0100

    factorize signals

--- geostore/signals.py
+++ geostore/signals.py
@@ -4,26 +4,24 @@ from geostore import settings as app_settings
 from geostore.tasks import feature_update_relations_destinations, layer_relations_set_destinations
 
 
-def save_feature(sender, instance, **kwargs):
-    def execute():
-        # update relations
-        feature_update_relations_destinations.delay(instance.pk) \
-            if app_settings.GEOSTORE_RELATION_CELERY_ASYNC else feature_update_relations_destinations(instance.pk)
-
+def execute_async_func(async_func, args=()):
+    """ Celery worker can be out of transaction, and raise DoesNotExist """
     if not transaction.get_connection().in_atomic_block:
-        execute()
+        async_func.delay(*args)
     else:
-        transaction.on_commit(lambda: execute())
+        transaction.on_commit(lambda: async_func.delay(*args))
+
+
+def save_feature(sender, instance, **kwargs):
+    if app_settings.GEOSTORE_RELATION_CELERY_ASYNC:
+        execute_async_func(feature_update_relations_destinations, (instance.pk,))
+    else:
+        feature_update_relations_destinations(instance.pk)
 
 
 def save_layer_relation(sender, instance, **kwargs):
-    def execute():
-        layer_relations_set_destinations.delay(instance.pk) \
-            if app_settings.GEOSTORE_RELATION_CELERY_ASYNC else layer_relations_set_destinations(instance.pk)
-
-    # update relations
     if kwargs.get('created', False):
-        if not transaction.get_connection().in_atomic_block:
-            execute()
+        if app_settings.GEOSTORE_RELATION_CELERY_ASYNC:
+            execute_async_func(layer_relations_set_destinations, (instance.pk,))
         else:
-            transaction.on_commit(lambda: execute())
+            layer_relations_set_destinations(instance.pk)
