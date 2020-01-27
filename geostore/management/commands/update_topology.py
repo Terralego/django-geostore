716e838304ac119a835fa8ee550b0d81195057f0
J-E Castagnede <j.e.castagnede@gmail.com>
2020-01-27 15:18:28 +0100

    improve tolerance for snapping routing topologies

--- geostore/management/commands/update_topology.py
+++ geostore/management/commands/update_topology.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.db import transaction
+from django.utils.translation import gettext as _
 
 from geostore.models import Layer
 from geostore.routing.helpers import Routing
@@ -17,7 +18,12 @@ class Command(BaseCommand):
                             help=("PK of the layer where to insert"
                                   "the features.\n"
                                   ))
-
+        parser.add_argument('-t',
+                            '--tolerance',
+                            type=float,
+                            action="store",
+                            required=False,
+                            help=_("Tolerance for snapping topologies (default to 0.00001)."))
         parser.add_argument('--dry-run',
                             action="store_true",
                             help='Execute une dry-run mode')
@@ -25,6 +31,7 @@ class Command(BaseCommand):
     @transaction.atomic()
     def handle(self, *args, **options):
         layer_pk = options.get('layer_pk', None)
+        tolerance = options.get('tolerance', 0.00001)
         dryrun = options.get('dry_run', None)
 
         sp = transaction.savepoint()
@@ -32,7 +39,7 @@ class Command(BaseCommand):
             layer = Layer.objects.get(pk=layer_pk)
         except Layer.DoesNotExist:
             raise CommandError(f"Layer with pk {layer_pk} doesn't exist")
-        if Routing.create_topology(layer):
+        if Routing.create_topology(layer, tolerance=tolerance):
             if options['verbosity'] >= 1:
                 self.stdout.write('Topology successfully updated')
         else:
