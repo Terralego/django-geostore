eb1a8bf633ea4a69cf231f5fb39efdf465aa0ff1
J-E Castagnede <j.e.castagnede@gmail.com>
2020-01-28 15:44:33 +0100

    hide pgrouting internal fields

--- geostore/routing/db/mixins.py
+++ geostore/routing/db/mixins.py
@@ -4,10 +4,12 @@ from django.db import models
 class PgRoutingMixin(models.Model):
     source = models.IntegerField(null=True,
                                  blank=True,
-                                 help_text='Internal field used by pgRouting')
+                                 help_text='Internal field used by pgRouting',
+                                 editable=False)
     target = models.IntegerField(null=True,
                                  blank=True,
-                                 help_text='Internal field used by pgRouting')
+                                 help_text='Internal field used by pgRouting',
+                                 editable=False)
 
     class Meta:
         abstract = True
