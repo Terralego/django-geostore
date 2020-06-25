a9ddbb98ff78deafa18e7a09c41d25a7a5964849
J-E Castagnede <j.e.castagnede@gmail.com>
2020-06-25 14:38:35 +0200

    enable django-rest-framework by default in test project / development mode

--- test_geostore/urls.py
+++ test_geostore/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+import debug_toolbar
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('geostore.urls')),
+]
+urlpatterns += [
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
