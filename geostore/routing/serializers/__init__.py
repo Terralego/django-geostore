64ca978c73404bf71face03e3a1066a69022c7cc
J-E Castagnede <j.e.castagnede@gmail.com>
2020-09-22 14:07:25 +0200

    update serializer to keep documentation in openapi

--- geostore/routing/serializers/__init__.py
+++ geostore/routing/serializers/__init__.py
@@ -5,10 +5,16 @@ from rest_framework.exceptions import ValidationError
 from rest_framework_gis import serializers as geo_serializers
 
 
-class RoutingSerializer(serializers.Serializer):
+class RoutingRequestSerializer(serializers.Serializer):
     geom = geo_serializers.GeometryField(help_text=_("A linestring with ordered waypoints."))
     callback_id = serializers.CharField(required=False,
                                         help_text=_("Optional callback id to match with your request."))
+
+
+class RoutingSerializer(RoutingRequestSerializer):
+    request = RoutingRequestSerializer(read_only=True)
+    route = serializers.JSONField(read_only=True)
+    way = geo_serializers.GeometryField(read_only=True)
 
     def validate_geom(self, value):
         if not isinstance(value, LineString):
