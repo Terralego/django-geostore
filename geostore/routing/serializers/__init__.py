e7775635d8be66d72cea26c38b1643ab60837a3d
J-E Castagnede <j.e.castagnede@gmail.com>
2020-09-22 14:25:39 +0200

    update serializer to keep documentation in openapi

--- geostore/routing/serializers/__init__.py
+++ geostore/routing/serializers/__init__.py
@@ -7,13 +7,17 @@ from rest_framework_gis import serializers as geo_serializers
 
 class RoutingRequestSerializer(serializers.Serializer):
     geom = geo_serializers.GeometryField(help_text=_("A linestring with ordered waypoints."),
-                                         write_only=True)
-    callback_id = serializers.CharField(required=False, write_only=True,
+                                         read_only=True)
+    callback_id = serializers.CharField(required=False, read_only=True,
                                         help_text=_("Optional callback id to match with your request."))
 
 
 class RoutingSerializer(RoutingRequestSerializer):
     request = RoutingRequestSerializer(read_only=True)
+    geom = geo_serializers.GeometryField(help_text=_("A linestring with ordered waypoints."),
+                                         write_only=True)
+    callback_id = serializers.CharField(required=False, write_only=True,
+                                        help_text=_("Optional callback id to match with your request."))
     route = serializers.JSONField(read_only=True)
     way = geo_serializers.GeometryField(read_only=True)
 
