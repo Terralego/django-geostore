34ebf54a6225041fe7d1564af6c8eec491079ba5
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2020-10-09 16:11:58 +0200

    Remove synchro routing without celery

--- geostore/routing/signals.py
+++ geostore/routing/signals.py
@@ -4,9 +4,5 @@ from geostore.routing.tasks import feature_update_routing
 
 
 def feature_routing(sender, instance, **kwargs):
-    if instance.layer.routable:
-        if app_settings.GEOSTORE_ROUTING_CELERY_ASYNC:
-            execute_async_func(feature_update_routing, (instance.pk,))
-        else:
-            feature_update_routing(instance)
-
+    if app_settings.GEOSTORE_ROUTING_CELERY_ASYNC:
+        execute_async_func(feature_update_routing, (instance,))
