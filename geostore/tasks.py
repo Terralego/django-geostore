cf43e77807e90ccba13e20d94027d930b4300ba0
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2020-11-13 10:07:51 +0100

    Fix indent

--- geostore/tasks.py
+++ geostore/tasks.py
@@ -43,7 +43,8 @@ def send_mail_async(user, path=None):
     html_content = html.render(context)
     txt = get_template('geostore/emails/{}.txt'.format(template_email))
     txt_content = txt.render(context)
-    send_mail(_('Export ready'), txt_content, None, [getattr(user, user.get_email_field_name())], html_message=html_content, fail_silently=True)
+    send_mail(_('Export ready'), txt_content, None, [getattr(user, user.get_email_field_name())],
+              html_message=html_content, fail_silently=True)
 
 
 def save_generated_file(user_id, layer_name, format_file, string_file):
