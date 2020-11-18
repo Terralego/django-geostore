0702445d1a506081c04fd5c31458e49181f6bf86
J-E Castagnede <j.e.castagnede@gmail.com>
2020-11-18 10:21:48 +0100

    improve translations

--- test_geostore/settings.py
+++ test_geostore/settings.py
@@ -14,8 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.join(BASE_DIR, 'test_geostore')
 LOCALE_PATHS = [
-    os.path.join(BASE_DIR, 'locale'),
+    os.path.join(PROJECT_DIR, 'locale'),
 ]
 
 # Quick-start development settings - unsuitable for production
