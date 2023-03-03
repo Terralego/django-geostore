f1a1e6b8817c8f62f966076bbdcfd9b1b6a99c31
J-E Castagnede <j.e.castagnede@gmail.com>
2023-03-03 16:33:45 +0100

    set default zoom levels to 0-23

--- geostore/settings.py
+++ geostore/settings.py
@@ -2,8 +2,8 @@ from django.conf import settings
 
 TERRA_TILES_HOSTNAMES = getattr(settings, 'TERRA_TILES_HOSTNAMES', [])
 
-MAX_TILE_ZOOM = getattr(settings, 'MAX_TILE_ZOOM', 15)
-MIN_TILE_ZOOM = getattr(settings, 'MIN_TILE_ZOOM', 10)
+MAX_TILE_ZOOM = getattr(settings, 'MAX_TILE_ZOOM', 23)
+MIN_TILE_ZOOM = getattr(settings, 'MIN_TILE_ZOOM', 0)
 
 INTERNAL_GEOMETRY_SRID = getattr(settings, 'INTERNAL_GEOMETRY_SRID', 4326)
 GEOSTORE_RELATION_CELERY_ASYNC = getattr(settings, 'GEOSTORE_RELATION_CELERY_ASYNC', False)
