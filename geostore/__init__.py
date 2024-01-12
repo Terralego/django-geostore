bd68edaac76b9524b5fef01d1f63bc4218a226dd
J-E Castagnede <j.e.castagnede@gmail.com>
2024-01-12 15:10:13 +0100

    fix test

--- geostore/__init__.py
+++ geostore/__init__.py
@@ -4,19 +4,26 @@ default_app_config = 'geostore.apps.GeostoreConfig'
 
 
 class GeometryTypes(IntegerChoices):
-    Point = 0
-    LineString = 1
+    Point = 0, 'Point'
+    LineString = 1, 'LineString'
     # LinearRing 2
-    Polygon = 3
-    MultiPoint = 4
-    MultiLineString = 5
-    MultiPolygon = 6
-    GeometryCollection = 7
+    Polygon = 3, 'Polygon'
+    MultiPoint = 4, 'MultiPoint'
+    MultiLineString = 5, 'MultiLineString'
+    MultiPolygon = 6, 'MultiPolygon'
+    GeometryCollection = 7, 'GeometryCollection'
+
+    @classmethod
+    def shape_allowed_types(cls):
+        """
+        Types allowed in shapefile export
+        """
+        return [cls.Point, cls.LineString, cls.Polygon, cls.MultiPoint,
+                cls.MultiLineString, cls.MultiPolygon]
 
     @classmethod
     def shape_allowed_type_names(cls):
         """
-        Types allowed in shapefile export
+        Name types allowed in shapefile export
         """
-        excluded = [cls.GeometryCollection]
-        return [geom_type for geom_type in cls if geom_type not in excluded]
+        return [geom_type.label for geom_type in cls.shape_allowed_types()]
